ad2e43ab73895fc3793caaee89ace55e4da78939
dofenschmitt <essolegrand123@gmail.com>
2021-01-15 12:28:54 +0300

    firebase-infos

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBedqpd-Ak1hnlRTwyTRYBxuOIapK680Sk",
+    authDomain: "e-plus-clone.firebaseapp.com",
+    projectId: "e-plus-clone",
+    storageBucket: "e-plus-clone.appspot.com",
+    messagingSenderId: "897276026933",
+    appId: "1:897276026933:web:0be63e6616ba0472cdd17c",
+    measurementId: "G-Y2KJP7H7Q4"
+  };
